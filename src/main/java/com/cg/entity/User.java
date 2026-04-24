a62b5e121b0f196d35a762e81d94bfd8ef92e1d2
singhal-ansh <anshsinghal0507@gmail.com>
2026-04-24 15:08:48 +0530

    made User and Vendor entities

---
+++ src/main/java/com/cg/entity/User.java
@@ -0,0 +1,68 @@
+package com.cg.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    private BigDecimal balance;
+
+    private LocalDateTime createdAt;
+}
