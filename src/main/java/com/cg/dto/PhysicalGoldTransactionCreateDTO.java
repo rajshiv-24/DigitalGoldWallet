ec8314b91aaa4ec444324a1dda445f73c3c7c62a
singhal-ansh <anshsinghal0507@gmail.com>
2026-04-27 15:53:37 +0530

    created 4 dtos

---
+++ src/main/java/com/cg/dto/PhysicalGoldTransactionCreateDTO.java
@@ -0,0 +1,19 @@
+package com.cg.dto;
+
+import java.math.BigDecimal;
+
+public class PhysicalGoldTransactionCreateDTO {
+    private Integer userId;
+    private Integer branchId;
+    private BigDecimal quantity;
+    private Integer deliveryAddressId;
+
+    public Integer getUserId() { return userId; }
+    public void setUserId(Integer userId) { this.userId = userId; }
+    public Integer getBranchId() { return branchId; }
+    public void setBranchId(Integer branchId) { this.branchId = branchId; }
+    public BigDecimal getQuantity() { return quantity; }
+    public void setQuantity(BigDecimal quantity) { this.quantity = quantity; }
+    public Integer getDeliveryAddressId() { return deliveryAddressId; }
+    public void setDeliveryAddressId(Integer deliveryAddressId) { this.deliveryAddressId = deliveryAddressId; }
+}
