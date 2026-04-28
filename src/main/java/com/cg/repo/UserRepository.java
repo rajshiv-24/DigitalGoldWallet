6e1021dd4d093fe24bf765311f23c18639a02ada
Harshit7112 <harshmaurya7112@gmail.com>
2026-04-28 10:42:26 +0530

    updated userrepo

--- src/main/java/com/cg/repo/UserRepository.java
+++ src/main/java/com/cg/repo/UserRepository.java
@@ -1,7 +1,9 @@
 package com.cg.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.cg.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+	Optional<User> findByEmail(String email);
 }
