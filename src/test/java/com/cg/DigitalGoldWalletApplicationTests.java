165c6ae7f3f10a1b2f3c8640905c26784fee5987
Deepanshu060 <paldipanshu060@gmail.com>
2026-05-04 11:49:54 +0530

    fix

--- src/test/java/com/cg/DigitalGoldWalletApplicationTests.java
+++ src/test/java/com/cg/DigitalGoldWalletApplicationTests.java
@@ -1,13 +1,32 @@
+//package com.cg;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest(properties = {
+//	    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+//	})
+//class DigitalGoldWalletApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
 package com.cg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=" +
+    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class DigitalGoldWalletApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
+
